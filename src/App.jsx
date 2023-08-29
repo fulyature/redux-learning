012a5190e0d8234308059b0654ee3473f541ffa4
fulya_ture <fulyature@gmail.com>
2023-08-29 21:02:52 +0300

    cunsuming

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Counter from "./components/counter/Counter";
 import Todo from "./components/todo/Todo";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
   return (
     <div className="app">
-      <Provider>
+      <Provider store={store}>
         {" "}
         <Counter />
         <Todo />
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,21 @@
-import "./Counter.css"
+import { useSelector } from "react-redux";
+import "./Counter.css";
 
 const Counter = () => {
+  // global statenın okunması
+
+  const count = useSelector((state) => state.count);
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
-      <h1>counter:0</h1>
+      <h1>counter:{count}</h1>
       <div>
         <button className="counter-button positive">increase</button>
         <button className="counter-button zero">reset</button>
         <button className="counter-button negative">decrease</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
