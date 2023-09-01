255fc973b485c26b37db85a66ac1af859ee41834
fulya_ture <fulyature@gmail.com>
2023-09-01 14:16:42 +0300

    onClickEvents

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,18 +1,34 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Counter.css";
 
 const Counter = () => {
   // global statenın okunması
 
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
       <h1>counter:{count}</h1>
       <div>
-        <button className="counter-button positive">increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button
+          className="counter-button positive"
+          onClick={() => dispatch({ type: "INC" })}
+        >
+          increase
+        </button>
+        <button
+          className="counter-button zero"
+          onClick={() => dispatch({ type: "CLR" })}
+        >
+          reset
+        </button>
+        <button
+          className="counter-button negative"
+          onClick={() => dispatch({ type: "DEC" })}
+        >
+          decrease
+        </button>
       </div>
     </div>
   );
